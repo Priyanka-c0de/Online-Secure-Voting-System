d009abb129cdbda6b2a2cf443c75ba8a0ef99a95
Priyanka Ganesan <priyankaganesan87@gmail.com>
2026-08-12 16:13:31 +0000

    Allow Render hostname

--- voting/settings.py
+++ voting/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-n@dc4$jy57^s_3^&#-f_g(98()_phy)6o2k#7lrw&it)v1s$aq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME", "localhost")]
 
 
 # Application definition
